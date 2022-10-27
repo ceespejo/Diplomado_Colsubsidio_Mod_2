2d64f6c4c440d622435056d45bed2bd7e9b8eb6a
ceespejo <113405605+ceespejo@users.noreply.github.com>
2022-10-26 20:00:58 -0500

    Scrip

---
+++ script.js
@@ -0,0 +1,16 @@
+/*console.log ("C is fun")
+console.log ("Phyton is Cool")
+console.log ("Java Script is amaizing")
+*/
+function primera(argumento, argumento1){
+    if (5===5) {
+        console.log(argumento)
+       
+     
+    }
+    else{
+        console.log("Pepito")
+    }
+}
+
+primera("hola")
